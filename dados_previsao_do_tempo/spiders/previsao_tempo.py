323e1ce1d4436b97f5a647c18f0272ffa37f74f2
MachadoProgrammer <gabrieldiasmachado@gmail.com>
2024-07-11 13:15:30 -0300

    commit inicial

---
+++ dados_previsao_do_tempo/spiders/previsao_tempo.py
@@ -0,0 +1,63 @@
+import scrapy
+import smtplib
+from email.message import EmailMessage
+import schedule
+
+class PrevisaoTempoSpider(scrapy.Spider):
+  name = 'previsao_tempo'
+  def start_requests(self):
+    # Abrir um site de previsão de tempo e acessa-ló
+    urls = [
+      'https://www.tempo.com/duque-de-caxias.htm'
+    ]
+    for url in urls:
+      yield scrapy.Request(url=url, callback=self.parse)
+
+
+# Coletar os dados meteorologicos   
+#  - Extrair a temperatura atual.
+#  - Extrair a condição do tempo atual (ex. ensolarado, nublado, etc.).
+#  - Extrair a previsão para os próximos 3 dias (temperatura e condição do tempo)
+
+  def parse(self, response):
+    contador = 0  # Inicializa um contador
+    for tempo in response.xpath('//span[@class="col day_col"]'):
+        if contador < 4:  # Verifica se ainda não atingiu o limite de 3 dias
+            yield {
+                'dia_atual': tempo.xpath('.//span[@class="subtitle-m"]/text()').get(),
+                'temperatura_maxima_e_minima': tempo.xpath('.//span[@class="max changeUnitT"]/text()').get() + " / " + tempo.xpath('.//span[@class="min changeUnitT"]/text()').get(),
+                'condicao_atual': tempo.xpath('.//img[@class="simbW"]/@alt').get()
+            }
+            contador += 1  # Incrementa o contador
+        else:
+            break  # Sai do loop se já processou 3 dias
+
+# Não vejo necessidade de tratar os dados, pois retornam no formato que eu desejo.
+
+# CONFIGURAÇÕES DE ENVIO DE EMAIL
+EMAIL_ADDRESS = 'xxxxxxxxxxxxxxxxxxxx'
+EMAIL_PASSWORD = 'xxxxxxxxxxxxx'
+
+# Enviar um email com os dados coletados
+
+mail = EmailMessage()
+mail['Subject'] = 'Previsão do tempo'
+mensagem = 'Previsão do tempo para os próximos 3 dias: \n\n'
+mail['From'] = EMAIL_ADDRESS
+mail['To'] = 'xxxxxxxxxxxxxxxxxxxxx'
+mail.add_header('Content-Type', 'text/plain')
+mail.set_payload(mensagem.encode('utf-8'))
+
+# Anexar os dados coletados ao email
+with open('dados.csv', 'rb') as f:
+    dados = f.read()
+    nome_arquivo = f.name
+    mail.add_attachment(dados, filename=nome_arquivo, maintype='application', subtype='octet-stream')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(mail)
+
+# fazer o agendamento do script 
+
+schedule.every().day.at("08:00").do(PrevisaoTempoSpider)
