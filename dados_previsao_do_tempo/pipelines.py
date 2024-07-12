216284d2fdca4232c965bc1f432f6da853cb7656
MachadoProgrammer <gabrieldiasmachado@gmail.com>
2024-07-12 20:50:54 -0300

    adicionado save no excel

--- dados_previsao_do_tempo/pipelines.py
+++ dados_previsao_do_tempo/pipelines.py
@@ -6,8 +6,27 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import openpyxl
+from dados_previsao_do_tempo.settings import XLSX_PATH
+
+CAMPOS = ['dia_atual', 'temperatura_maxima_e_minima', 'condicao_atual']
+class DadosXLSXPrevisaoDoTempoPipeline:
+    planilha = None
+    sheet = None
+
+    def open_spider(self, spider):
+        self.planilha = openpyxl.Workbook() 
+        self.sheet = self.planilha.active
+        self.sheet.append(CAMPOS) 
 
 
-class DadosPrevisaoDoTempoPipeline:
+
     def process_item(self, item, spider):
+        adpater = ItemAdapter(item)
+        self.sheet.append([adpater.get('dia_atual'), adpater.get('temperatura_maxima_e_minima'), adpater.get('condicao_atual')])
+        
         return item
+    
+    def close_spider(self, spider):
+        self.planilha.save(XLSX_PATH)
+        self.planilha.close()
